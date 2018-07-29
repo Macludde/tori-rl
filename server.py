7b319ffb8a1351b44774e29a51b0c28ddd640321
Ludvig Svedberg <ludvig.svedberg@mac.com>
2018-07-29 19:07:38 +0200

    Changed to python server

--- server.py
+++ server.py
@@ -2,7 +2,7 @@ import socket               # Import socket module
 
 s = socket.socket()         # Create a socket object
 host = socket.gethostname() # Get local machine name
-port = 12345                # Reserve a port for your service.
+port = 7051                # Reserve a port for your service.
 s.bind((host, port))        # Bind to the port
 RECV_BUFFER = 4096 # Advisable to keep it as an exponent of 2
 
