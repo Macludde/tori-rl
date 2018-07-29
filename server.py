8b33d3155415979817fdc20feb722fd18ff9cc94
BuildTools <unconfigured@null.spigotmc.org>
2018-07-29 19:18:42 +0200

    Made all names longer in the server.py file

--- server.py
+++ server.py
@@ -1,19 +1,20 @@
 import socket               # Import socket module
 
-s = socket.socket()         # Create a socket object
+socke = socket.socket()         # Create a socket object
 host = socket.gethostname() # Get local machine name
-port = 7051                # Reserve a port for your service.
-s.bind((host, port))        # Bind to the port
+port = 7051                 # Reserve a port for your service.
+socket.bind((host, port))        # Bind to the port
 RECV_BUFFER = 4096 # Advisable to keep it as an exponent of 2
 
 print("Server started. Listening on port", port)
-s.listen(5)                 # Now wait for client connection.
+
+socket.listen(5)                 # Now wait for client connection.
 while True:
-   c, addr = s.accept()     # Establish connection with client.
-   print('Got connection from', addr)
-   c.send('Thank you for connecting'.encode('utf-8'))
-   while 1:
-	   data = c.recv(RECV_BUFFER)
-	   print(data)
-	   if data=='exit':
-		   c.close()                # Close the connection
+    client, addr = socket.accept()    # Establish connection with client.
+    print('Got connection from', addr)
+    client.send('Thank you for connecting'.encode('utf-8'))
+    while True:
+        data = client.recv(RECV_BUFFER)
+        print(data)
+        if data == 'exit':
+            client.close()                # Close the connection
