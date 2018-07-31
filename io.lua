d903ec92b9b54311a251f3826adab97aa45de850
qwesz9090 <41839502+qwesz9090@users.noreply.github.com>
2018-07-31 20:56:21 +0200

    Update io.lua

--- io.lua
+++ io.lua
@@ -20,12 +20,15 @@ local function write_file(filename)
 local function append_to_file(filename,string)
    local tmpInput = open_file(filename)
    local lines = {}
-   end
+ end
+ 
  local function read_from_Py()
- 	tmpFileRead = open_file(CommsRead)
-	vars = tmpFileRead:read("*l")
-
-	return vars
+ 	file = open_file(CommsRead)
+	data = file:read("*l")
+	file:close()
+	file = write_file()
+	file:write("")
+	return data
 end
  
  local function send_to_Py(vars)
@@ -34,6 +37,23 @@ end
 	 tmpFile:close()
 	 echo("done")
 end
+int = 0
+while true do
+	int = int + 1
+	send_to_Py(int)
+	while true do
+		temp = read_from_Py()
+		if (temp ~= nil) then
+			break
+		end
+	end
+	if (int > 1000000) then 
+		break
+	end
+end
+	
+	
+	
 
 
 
