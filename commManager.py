f4a7db07c90fe3fe790b8c5c050fa17c5ad3518a
Ludvig Svedberg <ludvig.svedberg@mac.com>
2018-08-01 10:12:27 +0200

    Something

--- commManager.py
+++ commManager.py
@@ -1,7 +1,6 @@
 def readComms():
 	commsPy = open("commsPy.txt","r+")
 	line = commsPy.readline()
-	print(line)
 	# Process Line
 	commsPy.truncate(0);
 	commsPy.close()
@@ -12,11 +11,3 @@ def writeComms(data):
 	commsLua = open("commsLua.txt", "a")
 	commsLua.write(data) 
 	commsLua.close()
-
-
-while True:
-	writeComms("move:3,2,1,0,2,0,3,1,0,1,1,2")
-	while True:
-		line = readComms()
-		if line != "":
-			break;
--- io.lua
+++ io.lua
@@ -1,39 +1,32 @@
 CommsWrite = "commsPy.txt"
 CommsRead = "commsLua.txt"
 
-local function open_file(filename)
-        local oldinput = io.input()
-	io.input(filename)
-	local file = io.input()
-	io.input(oldinput)
-	return file
-     end
-
-local function write_file(filename)
-	local oldoutput = io.output()
-	io.output(filename)
-	local file = io.output()
-	io.output(oldoutput)
-	return file
-     end
-
 local function append_to_file(filename,string)
    local tmpInput = open_file(filename)
    local lines = {}
-   end
+ end
+ 
  local function read_from_Py()
- 	tmpFileRead = open_file(CommsRead)
-	vars = tmpFileRead:read("*l")
+ 	file = io.open(CommsRead, "r+")	
+ 	io.input(file)
+ 	data = io.read("*l")
 
-	return vars
+	io.output(file)
+	io.write("")
+	io.close(file)
+
+	return data
 end
  
- local function send_to_Py(vars)
- tmpFile = write_file(CommsWrite)
-	 tmpFile:write(vars)
-	 tmpFile:close()
-	 echo("done")
+ local function send_to_Py(data)
+ 	file = io.open(CommsWrite, "w")	
+	io.output(file)
+	io.write(data)
+	io.close(file)
 end
 
 
-
+send_to_Py("Test")
+echo(read_from_Py())
+echo(read_from_Py())
+echo(read_from_Py())
