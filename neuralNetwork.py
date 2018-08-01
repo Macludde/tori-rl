f4edea5320b8c029c952a45275ee501ab0bf90b0
Ludvig Svedberg <ludvig.svedberg@mac.com>
2018-08-01 15:57:56 +0200

    Fixed neural network predictions

--- neuralNetwork.py
+++ neuralNetwork.py
@@ -2,8 +2,9 @@ import controller
 from keras.layers import Dense, Dropout
 from keras.models import Sequential
 import numpy as np
+import random
 
-class AI():
+class Agent():
 	def __init__(self):
 		self.learning_rate = 0.95
 		self.y = 0.95
@@ -13,15 +14,9 @@ class AI():
 		self.memory = []
 
 	def createModel(self, output):
-
 	    # Neural Net for Deep-Q learning Model
-	    model = Sequential()
-	    model.add(Dense(24, input_dim=self.state_size, activation='relu'))
-	    model.add(Dense(24, activation='relu'))
-	    model.add(Dense(self.action_size, activation='linear'))
-	    model.compile(loss='mse', optimizer=Adam(lr=self.learning_rate))
 		model = Sequential()
-		model.add(Dense(512, input_shape=(132,), activation="relu"))
+		model.add(Dense(512, input_dim=132, activation="relu"))
 		model.add(Dropout(0.3))
 
 		model.add(Dense(1024, activation="relu"))
@@ -44,7 +39,21 @@ class AI():
 		memory.append((state, action, reward, next_state, done))
 
 	def setup(self): 
-		self.model = createModel()
+		self.model = self.createModel(88)
+
+	def act(self, state):
+		moves = []
+		if np.random.rand() <= self.exploration_rate:
+			for i in range(22):
+				moves.append(random.randint(0, 3))
+			return moves
+		act_values = self.model.predict(state)[0]
+		for i in range(22):
+			moves.append(np.argmax(act_values[i:i+4])%4)
+
+		return moves
+
+
 
 	def turn(self):
 
@@ -53,4 +62,20 @@ class AI():
 		dataPoints = [float(dataPoint) for dataPoint in data.split(",")]
 		if (self.last_state != []):
 			target = dataPoints[-1] + y * np.amax(model.predict(dataPoints[:-1]))
+	def predict(self):
+		controller.setRandomMuscles()
+		data = controller.getData()
+		data = data[5:]
+		dataPoints = np.array([[float(dataPoint) for dataPoint in data.split(",")]])
+		print(self.act(dataPoints))
+
+
+
+class NeuralNet():
+	def __init__(self):
+		pass
+
+agent = Agent()
+agent.setup()
+agent.predict()
 
