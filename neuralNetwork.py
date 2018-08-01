8903b2177cf2133a5dd4b770f2dc8a4d7bf2f049
Ludvig Svedberg <ludvig.svedberg@mac.com>
2018-08-01 15:20:13 +0200

    a

--- neuralNetwork.py
+++ neuralNetwork.py
@@ -3,72 +3,54 @@ from keras.layers import Dense, Dropout
 from keras.models import Sequential
 import numpy as np
 
-learning_rate = 0.95
-y = 0.95
-exploration_rate = 0.5
-explorations_decay = 0.999
-episodes = 10000
-memory = []
+class AI():
+	def __init__(self):
+		self.learning_rate = 0.95
+		self.y = 0.95
+		self.exploration_rate = 0.5
+		self.explorations_decay = 0.999
+		self.episodes = 10000
+		self.memory = []
 
-def createModel(output):
+	def createModel(self, output):
 
-    # Neural Net for Deep-Q learning Model
-    model = Sequential()
-    model.add(Dense(24, input_dim=self.state_size, activation='relu'))
-    model.add(Dense(24, activation='relu'))
-    model.add(Dense(self.action_size, activation='linear'))
-    model.compile(loss='mse', optimizer=Adam(lr=self.learning_rate))
-	model = Sequential()
-	model.add(Dense(512, input_shape=(132,), activation="relu"))
-	model.add(Dropout(0.3))
+	    # Neural Net for Deep-Q learning Model
+	    model = Sequential()
+	    model.add(Dense(24, input_dim=self.state_size, activation='relu'))
+	    model.add(Dense(24, activation='relu'))
+	    model.add(Dense(self.action_size, activation='linear'))
+	    model.compile(loss='mse', optimizer=Adam(lr=self.learning_rate))
+		model = Sequential()
+		model.add(Dense(512, input_shape=(132,), activation="relu"))
+		model.add(Dropout(0.3))
 
-	model.add(Dense(1024, activation="relu"))
-	model.add(Dropout(0.3))
+		model.add(Dense(1024, activation="relu"))
+		model.add(Dropout(0.3))
 
-	model.add(Dense(2048, activation="relu"))
-	model.add(Dropout(0.3))
+		model.add(Dense(2048, activation="relu"))
+		model.add(Dropout(0.3))
 
-	model.add(Dense(1024, activation="relu"))
-	model.add(Dropout(0.3))
+		model.add(Dense(1024, activation="relu"))
+		model.add(Dropout(0.3))
 
-	model.add(Dense(512, activation="relu"))
-	model.add(Dropout(0.3))
-	model.add(Dense(output, activation="sigmoid"))
+		model.add(Dense(512, activation="relu"))
+		model.add(Dropout(0.3))
+		model.add(Dense(output, activation="sigmoid"))
 
-	model.compile(loss='mse', optimizer='adam', metrics=['mae'])
-	return model
+		model.compile(loss='mse', optimizer='adam', metrics=['mae'])
+		return model
 
-def remember(state, action, reward, next_state, done):
-	memory.append((state, action, reward, next_state, done))
+	def remember(self, state, action, reward, next_state, done):
+		memory.append((state, action, reward, next_state, done))
 
-def setup(): 
-	model = createModel()
-	global model
+	def setup(self): 
+		self.model = createModel()
 
-def turn():
+	def turn(self):
 
-	data = controller.getData()
-	data = data[5:]
-	dataPoints = [float(dataPoint) for dataPoint in data.split(",")]
-	if (last_state != []):
-		target = dataPoints[-1] + y * np.amax(model.predict(dataPoints[:-1]))
+		data = controller.getData()
+		data = data[5:]
+		dataPoints = [float(dataPoint) for dataPoint in data.split(",")]
+		if (self.last_state != []):
+			target = dataPoints[-1] + y * np.amax(model.predict(dataPoints[:-1]))
 
-
-
-
-	"""
-	y = 0.95
-	eps = 0.5
-	decay_factor = 0.999
-	r_avg_list = []
-	for i in range(num_episodes):
-	    s = env.reset()
-	    eps *= decay_factor
-	    if i % 100 == 0:
-	        print("Episode {} of {}".format(i + 1, num_episodes))
-	    done = False
-	    r_sum = 0
-	    while not done:
-	"""
-
-train()
