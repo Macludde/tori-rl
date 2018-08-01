db317b989b42e36f3856d10f00eca2088eca1606
Ludvig Svedberg <ludvig.svedberg@mac.com>
2018-08-01 15:12:39 +0200

    Created skeleton of neural network

--- neuralNetwork.py
+++ neuralNetwork.py
@@ -1,17 +1,23 @@
 import controller
 from keras.layers import Dense, Dropout
+from keras.models import Sequential
+import numpy as np
 
-def sigmoid(x):
-	return 1
-
-def setupModels():
-	models = []
-	for i in range(20):
-		models.append(createModel(4))
-	models.append(createModel(2))
-	models.append(createModel(2))
+learning_rate = 0.95
+y = 0.95
+exploration_rate = 0.5
+explorations_decay = 0.999
+episodes = 10000
+memory = []
 
 def createModel(output):
+
+    # Neural Net for Deep-Q learning Model
+    model = Sequential()
+    model.add(Dense(24, input_dim=self.state_size, activation='relu'))
+    model.add(Dense(24, activation='relu'))
+    model.add(Dense(self.action_size, activation='linear'))
+    model.compile(loss='mse', optimizer=Adam(lr=self.learning_rate))
 	model = Sequential()
 	model.add(Dense(512, input_shape=(132,), activation="relu"))
 	model.add(Dropout(0.3))
@@ -19,24 +25,37 @@ def createModel(output):
 	model.add(Dense(1024, activation="relu"))
 	model.add(Dropout(0.3))
 
+	model.add(Dense(2048, activation="relu"))
+	model.add(Dropout(0.3))
+
 	model.add(Dense(1024, activation="relu"))
 	model.add(Dropout(0.3))
 
 	model.add(Dense(512, activation="relu"))
 	model.add(Dropout(0.3))
+	model.add(Dense(output, activation="sigmoid"))
 
-	model.add(Dense(128, activation="relu"))
-	model.add(Dropout(0.3))
-	model.add(Dense(output, activation="softmax"))
-	model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['mae'])
+	model.compile(loss='mse', optimizer='adam', metrics=['mae'])
+	return model
 
-def train():
-	controller.setRandomMuscles()
+def remember(state, action, reward, next_state, done):
+	memory.append((state, action, reward, next_state, done))
+
+def setup(): 
+	model = createModel()
+	global model
+
+def turn():
+
 	data = controller.getData()
 	data = data[5:]
-	print(data)
-	dataPoints = data.split(",")
-	print(dataPoints)
+	dataPoints = [float(dataPoint) for dataPoint in data.split(",")]
+	if (last_state != []):
+		target = dataPoints[-1] + y * np.amax(model.predict(dataPoints[:-1]))
+
+
+
+
 	"""
 	y = 0.95
 	eps = 0.5
